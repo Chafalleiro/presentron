6a319685a36796b2bd2c982ac811578c4a40bae0
Alfonso <alfonsoabelenda@gmail.com>
2026-07-28 02:34:20 +0200

    Remove loadSlideImages function and related code

--- js/presentron.js
+++ js/presentron.js
@@ -126,38 +126,6 @@ async function askFileLoad(par,msg,st)
 //	return msg;
 }
 
-
-//************* Load slide images helper function ****************************************
-function loadSlideImages(slideArray, callback)
-{
-        var loadedCount = 0;
-        var totalSlides = slideArray.length;
-        
-        if(totalSlides === 0) {
-                callback();
-                return;
-        }
-        
-        for(var i = 0; i < totalSlides; i++) {
-                var img = new Image();
-                img.onload = function() {
-                        loadedCount++;
-                        if(loadedCount >= totalSlides) {
-                                console.log("All slide images loaded:", loadedCount);
-                                callback();
-                        }
-                };
-                img.onerror = function() {
-                        console.error("Failed to load slide image");
-                        loadedCount++;
-                        if(loadedCount >= totalSlides) {
-                                callback();
-                        }
-                };
-                img.src = slideArray[i]["slide"];
-        }
-}
-
 function imageLoads(arrImg,arrSrc,cnt)
 {
 	for(var i = 0; i < getLength(arrSrc); i++) {
@@ -213,16 +181,13 @@ async function startProyector(ndx)
 	//Load slides File from files
 	var mesg = await slideLoads(val.path,"slides");
 	console.log("mesg" , mesg);
-	cLoad = false; // Don't set to true yet - wait for images to load
+	cLoad = true;
 	if(nOk == 1)
 	{
 		clearInterval(waitForArr);
 		waitForArr = null;
-		waitForArr = setInterval(function ()
-		{
-			//Wait for file to load
-			if (swUpt == true)
-			{
+		waitForArr = setInterval(function () {//Wait for file to load
+			if (swUpt == true){
 				clearInterval(waitForArr);
 				waitForArr = null;
 				slIndexes = null;
@@ -236,36 +201,37 @@ async function startProyector(ndx)
 				val.path = actFile.name;
 				img_count = slIndexes.length;
 				let request = db.put("files", val); //Update the file name
-                // Load all slide images before showing the projector
-                loadSlideImages(anArr, function()
+
+				//Show the proyector screen.
+				document.getElementById("controls").style.visibility = "visible";
+				document.getElementById("slideText").style.visibility = "visible";
+				document.getElementById("controls").style.opacity = "100%";
+				document.getElementById("slideText").style.opacity = "100%";
+				//document.getElementById("slideText").innerHTML  = "LOADING SLIDES... " + ndx+"<br>file "+ val.path+"<br>slIndexes " + slIndexes + "<br>actFile.name " + actFile.name;
+				sw_on = true;
+	
+				document.getElementById("projector").style.visibility = "visible";
+				document.getElementById("projector").style.opacity = "100%";
+
+				sliFan.setAttribute("src","snd/encenderptoyectorvideo.mp3");
+				sliFan.autoplay = true;
+	
+				sliTrack.setAttribute("src","snd/meterdiaposcaja.mp3");
+				sliTrack.loop = true;
+				sliTrack.autoplay = true;
+	
+				const myTimeout = setTimeout(function (){stopLoadingSound(sliTrack);}, 5000);
+
+				const fanTimeout = setTimeout(function (){stopLoadingSoundAndStartOtherTrack(sliFan,"snd/ventiladorproyectorvideo.mp3");}, 150);
+	
+				sliEffects.setAttribute("src","snd/cambioDiapo.mp3");
+				sliEffects.autoplay = false;
+				if(mesg != true)
 				{
-                    // All images loaded successfully
-                    cLoad = true;
-                    //Show the proyector screen.
-                    document.getElementById("controls").style.visibility = "visible";
-                    document.getElementById("slideText").style.visibility = "visible";
-					document.getElementById("controls").style.opacity = "100%";
-					document.getElementById("slideText").style.opacity = "100%";
-					//document.getElementById("slideText").innerHTML  = "LOADING SLIDES... " + ndx+"<br>file "+ val.path+"<br>slIndexes " + slIndexes + "<br>actFile.name " + actFile.name;
-					sw_on = true;
-					document.getElementById("projector").style.visibility = "visible";
-					document.getElementById("projector").style.opacity = "100%";
-					sliFan.setAttribute("src","snd/encenderptoyectorvideo.mp3");
-					sliFan.autoplay = true;
-					sliTrack.setAttribute("src","snd/meterdiaposcaja.mp3");
-					sliTrack.loop = true;
-					sliTrack.autoplay = true;
-					const myTimeout = setTimeout(function (){stopLoadingSound(sliTrack);}, 5000);
-					const fanTimeout = setTimeout(function (){stopLoadingSoundAndStartOtherTrack(sliFan,'snd/ventiladorproyectorvideo.mp3');}, 150);
-					sliEffects.setAttribute("src","snd/cambioDiapo.mp3");
-					sliEffects.autoplay = false;
-					if(mesg != true)
-					{
-						showModal('alertW');
-						showModal('importData');
-					}
-					showSlide(0);
-				});
+					showModal('alertW');
+					showModal('importData');
+				}
+				showSlide(0);
 			}
 		}, 100);
 	}
